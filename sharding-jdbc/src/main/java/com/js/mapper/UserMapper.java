058bf83cfb10b84185200383557377c919f646a5
dujie <2545251075@qq.com>
2021-08-18 21:25:32 +0800

    fix

--- sharding-jdbc/src/main/java/com/js/mapper/UserMapper.java
+++ sharding-jdbc/src/main/java/com/js/mapper/UserMapper.java
@@ -1,16 +1,16 @@
 package com.js.mapper;
 
-import com.js.pojo.User;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
+import com.js.pojo.User;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Mapper;
 import java.util.List;
 
 /**
  * (User)表数据库访问层
  *
  * @author makejava
- * @since 2021-04-24 13:18:46
+ * @since 2021-08-17 21:27:55
  */
 @Mapper
 public interface UserMapper {
@@ -21,40 +21,40 @@ public interface UserMapper {
      * @param id 主键
      * @return 实例对象
      */
-    User selectById(@Param("id") Long id);
+    User findById(@Param("id") Long id);
 
 
     /**
      * 通过实体作为筛选条件查询
      *
-     * @param User 实例对象
+     * @param user 实例对象
      * @return 对象列表
      */
-    List<User> selectList(User User);
+    List<User> find(User user);
 
     /**
      * 新增数据
      *
-     * @param User 实例对象
+     * @param user 实例对象
      * @return 影响行数
      */
-    int insert(User User);
+    Integer insert(User user);
 
     /**
      * 批量新增
      *
-     * @param Users 实例对象的集合
+     * @param users 实例对象的集合
      * @return 影响行数
      */
-    int batchInsert(@Param("Users") List<User> Users);
+    Integer batchInsert(@Param("users") List<User> users);
 
     /**
      * 修改数据
      *
-     * @param User 实例对象
+     * @param user 实例对象
      * @return 影响行数
      */
-    int update(User User);
+    Integer updateById(User user);
 
     /**
      * 通过主键删除数据
@@ -62,36 +62,36 @@ public interface UserMapper {
      * @param id 主键
      * @return 影响行数
      */
-    int deleteById(@Param("id") Long id);
+    Integer deleteById(@Param("id") Long id);
 
     /**
      * 通过条件删除数据
      *
-     * @param id 主键
+     * @param user 删除条件
      * @return 影响行数
      */
-    int deleteByEntity(User User);
+    Integer deleteByEntity(User user);
 
     /**
-     * 通过条件删批量删除数据除数据
+     * 通过ID批量删除数据除数据
      *
-     * @param id 主键集合
+     * @param list 主键集合
      * @return 影响行数
      */
-    int deleteByIds(@Param("list") List<Long> list);
+    Integer deleteByIds(@Param("list") List<Long> list);
 
     /**
      * 条件查询总数
      *
      * @return 数据总数
      */
-    int countByEntity(User User);
+    Integer countByEntity(User user);
 
     /**
      * 批量修改
      *
      * @return 修改成功的条数
      */
-    int updateBatch(@Param("list") List<User> list);
+    Integer updateBatch(@Param("list") List<User> list);
 
 }
--- sharding-jdbc/src/main/java/com/js/pojo/User.java
+++ sharding-jdbc/src/main/java/com/js/pojo/User.java
@@ -2,15 +2,29 @@ package com.js.pojo;
 
 import lombok.Data;
 
+/**
+ * (User)实体类
+ *
+ * @author 渡劫 dujie
+ * @since 2021-08-17 21:27:53
+ */
 @Data
 public class User {
-
+    /**
+     * 主键
+     */
     private Long id;
-
-    private String username;
-
+    /**
+     * 用户名
+     */
+    private String userName;
+    /**
+     * 密码
+     */
     private String password;
-
+    /**
+     * 性别
+     */
     private String gender;
 
 }
