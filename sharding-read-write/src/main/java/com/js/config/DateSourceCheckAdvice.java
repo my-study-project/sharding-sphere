9fed60b96bf8dfcd446f56d6697e10ba9b44906b
姜爽 <2545251075@qq.com>
2021-09-25 17:45:08 +0800

    fix -> 读写分离从库随机路由

--- sharding-read-write/src/main/java/com/js/config/DateSourceCheckAdvice.java
+++ sharding-read-write/src/main/java/com/js/config/DateSourceCheckAdvice.java
@@ -3,11 +3,16 @@ package com.js.config;
 import lombok.extern.slf4j.Slf4j;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.apache.commons.lang3.RandomUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.shardingsphere.api.hint.HintManager;
+import org.apache.shardingsphere.core.yaml.config.masterslave.YamlMasterSlaveRuleConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.util.PatternMatchUtils;
+
+import java.util.List;
 
 @Component
 @Slf4j
@@ -15,6 +20,9 @@ public class DateSourceCheckAdvice implements MethodInterceptor {
 
     @Value("${spring.shardingsphere.datasource.onlyWritePattern}")
     private String onlyWritePattern;
+
+    @Autowired
+    private YamlMasterSlaveRuleConfiguration yamlMasterSlaveRuleConfiguration;
 
     @Override
     public Object invoke(MethodInvocation invocation) {
@@ -24,7 +32,10 @@ public class DateSourceCheckAdvice implements MethodInterceptor {
             if (checkMethod(methodName)) {
                 HintManager.getInstance().setMasterRouteOnly();
             } else {
-                HintManager.getInstance().setDatabaseShardingValue("read");
+                // 读库选择随机到一个从库上
+                List<String> slaveDataSourceNames = yamlMasterSlaveRuleConfiguration.getSlaveDataSourceNames();
+                int i = RandomUtils.nextInt(0, slaveDataSourceNames.size());
+                HintManager.getInstance().setDatabaseShardingValue(slaveDataSourceNames.get(i));
             }
             result = invocation.proceed();
         } catch (Exception e) {
