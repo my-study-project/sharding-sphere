861f8e62f90616e21b28a831fec33ef3edb7310c
姜爽 <jiangshuang@wejoydata.com>
2022-03-31 14:22:51 +0800

    记录json忽略

--- sharding-encrypt/src/main/java/com/js/pojo/User.java
+++ sharding-encrypt/src/main/java/com/js/pojo/User.java
@@ -1,5 +1,6 @@
 package com.js.pojo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 /**
@@ -9,6 +10,7 @@ import lombok.Data;
  * @since 2021-08-17 21:27:53
  */
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class User {
     /**
      * 主键
